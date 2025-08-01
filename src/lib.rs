d11f48b4e3fbeb60a22b9014c59b2d04d1cf4bc9
Cecilia-Windy <hy1415926535897932@outlook.com>
2025-08-01 13:07:14 +0800

    subscription && test

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,3 @@
-//! src/lib.rs
-
-use actix_web::{App, HttpResponse, HttpServer, web};
-use actix_web::dev::Server;
-use std::net::TcpListener;
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> std::io::Result<Server> {
-    let address = listener.local_addr().unwrap();
-
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-        })
-        .listen(listener)?
-        .run();
-
-    println!("Running on http://{address}");
-
-    Ok(server)
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
